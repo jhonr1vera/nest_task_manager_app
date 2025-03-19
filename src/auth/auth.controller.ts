c39f086c832f6b93a45436132341e94da5ac3968
Jhon Rivera <jhonariverab@gmail.com>
2025-03-19 18:07:58 -0400

    add Swagger documentation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,17 +2,24 @@ import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { ApiResponse, ApiOperation } from '@nestjs/swagger';
+
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+
+  @ApiOperation({summary: 'Create a new user'})
+  @ApiResponse({status: 200, description: "return user"})
   @Post('register')
     @UsePipes(new ValidationPipe()) //* validation pipe local
     create(@Body() createUserDto: CreateUserDto) {
       return this.authService.register(createUserDto);
   }
 
+  @ApiOperation({summary: 'Create a new user'})
+  @ApiResponse({status: 200, description: "return access token (bearer)"})
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
     return await this.authService.login(loginDto);
