766fdc498ab389b8680a7cbaa6cc5f846f320c73
Jhon Rivera <jhonariverab@gmail.com>
2025-03-19 13:45:25 -0400

    fixed comments resource

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,13 +5,12 @@ import { PrismaService } from 'src/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthGuard } from './auth.guard';
 import { RolesGuard } from './roles.guard';
-import { ROUTES } from '@nestjs/core/router/router-module';
 
 @Module({
     imports: [
         JwtModule.register({
             privateKey: process.env.JWT_SECRET,
-            signOptions: { expiresIn: '1h' },
+            signOptions: { expiresIn: '6h' },
         }),
     ],
     controllers: [AuthController],
