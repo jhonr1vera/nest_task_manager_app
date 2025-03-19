f7f630d43a45810dabd8c4368bcc487ae3c10147
Jhon Rivera <jhonariverab@gmail.com>
2025-03-19 19:00:14 -0400

    add swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,15 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+  .setTitle('Task Manager API')
+  .setDescription('Task Manager API with CRUD for Users, CRUD for Tasks and a CRUD to add comments to Tasks. This API includes validations and authorizations modules. Previous tested with ThunderClient and Postman, start by registering and logging to generate your token to perform actions')
+  .setVersion('1.0')
+  .addTag('tasks')
+  .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
