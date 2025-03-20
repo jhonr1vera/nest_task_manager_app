c5bcd3da1296d4568085f9e86dbb5a55125cf5ef
Jhon Rivera <jhonariverab@gmail.com>
2025-03-20 17:35:26 -0400

    add eventemitter module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
 import { CommentsModule } from './comments/comments.module';
 import { AuthModule } from './auth/auth.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { TaskHistoryModule } from './task-history/task-history.module';
 
 @Module({
-  imports: [TasksModule, UsersModule, CommentsModule, AuthModule],
+  imports: [TasksModule, UsersModule, CommentsModule, AuthModule, EventEmitterModule.forRoot(), TaskHistoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
